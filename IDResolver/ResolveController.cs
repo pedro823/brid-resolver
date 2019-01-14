450608bf9f67a4a10673d647dc4e2a6d5e75430e
Andrew Ijano <andrew.lopes@uol.com.br>
2019-01-14 00:14:52 -0200

    Adds ID resolving

---
+++ IDResolver/ResolveController.cs
@@ -0,0 +1,17 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using StackExchange.Redis;
+
+namespace IDResolver
+{
+    public class ResolveController : Controller
+    {
+        public async Task<IActionResult> ResolveId(string id)
+        {
+            var redis = ConnectionMultiplexer.Connect("localhost");
+            var db = redis.GetDatabase();
+            var callbackUrl = await db.StringGetAsync(id);
+            return Content(callbackUrl);
+        }
+    }
+}
